f217cdd2964c799acf5f913a07146ddb831d6cf7
rohit phadatare <rohitphadatare61@gmail.com>
2025-06-27 00:04:25 +0530

    second

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -11,7 +11,7 @@ export default function CreatePage() {
           <label className="block mb-1">Name</label>
           <input
             type="text"
-            name="Name"
+            name="name"
             required
             className="w-full border px-3 py-2 rounded"
           />
